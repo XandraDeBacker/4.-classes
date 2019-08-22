d2c000bc8cc4848cc7bca6348bc355936ceea086
angel <angelbecode@gmail.com>
2019-08-22 15:57:01 +0200

    HTML + SCSS in home.php

---
+++ Blackjack/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="background" id="background">
+            <div class="players">
+                <div class="DC">
+                    <div class="dealer">dealer</div>
+                    <div class="cardDealer">cardD</div>
+                </div>
+                <div class="PC">
+                    <div class="cardPlayer">cardP</div>
+                    <div class="player">player</div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="sidebar">
+            
+            <div class="scoreDisplay">
+                <div id="scoreDealer">scoreDealer</div>
+                
+            </div>
+            <div id="buttons">
+                <button id="hit">hit</button>
+                <button id="stand">stand</button>
+                <button id="surrender">give up</button>
+                <div id="scorePlayer">scorePlayer</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
