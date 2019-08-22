092499b760bf7264e4527a09a2bcea542e7e6261
debackerx <xdebacker.bc@gmail.com>
2019-08-22 13:57:23 +0200

    updated blackjack.php

---
+++ Blackjack/blackJack.php
@@ -0,0 +1,19 @@
+<?php
+class blackJack{
+public $score = 0;
+public $deck = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+
+function Hit(){
+
+}
+
+function Stand(){
+
+}
+
+function Surrender(){
+
+}
+}
+
+ ?>
