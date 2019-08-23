e2aabb130969a518d567309ba9668d1b1370e0d1
patricklibert <patrick.libert@gmail.com>
2019-08-23 16:11:46 +0200

    Third Card

--- Blackjack/blackJack.php
+++ Blackjack/blackJack.php
@@ -4,33 +4,38 @@ class blackJack {
     public $score = 0;
     public $playercard = "";
     public $dealercard = "";
+    public $random_cards;
+    public $random_cards1;
+    public $turn;
 
+    // array of playing cards
     public $cards = array("2C"=>2,"3C"=>3,"4C"=>4,"5C"=>5,"6C"=>6,"7C"=>7,"8C"=>8,"9C"=>9,"10C"=>10,"JC"=>10,"QC"=>10,"KC"=>10,"AC"=>11,
                     "2H"=>2,"3H"=>3,"4H"=>4,"5H"=>5,"6H"=>6,"7H"=>7,"8H"=>8,"9H"=>9,"10H"=>10,"JH"=>10,"QH"=>10,"KH"=>10,"AH"=>11,
                     "2S"=>2,"3S"=>3,"4S"=>4,"5S"=>5,"6S"=>6,"7S"=>7,"8S"=>8,"9S"=>9,"10S"=>10,"JS"=>10,"QS"=>10,"KS"=>10,"AS"=>11,
                     "2D"=>2,"3D"=>3,"4D"=>4,"5D"=>5,"6D"=>6,"7D"=>7,"8D"=>8,"9D"=>9,"10D"=>10,"JD"=>10,"QD"=>10,"KD"=>10,"AD"=>11);
-    function Deal(){
-       $this->cards;
+    // Deal two cards to player and dealer
+    public function Deal(){
+       //Player
+       // Take two random cards out of array
        $random_cards=array_rand($this->cards,2);
        $random_cards00=array_rand($this->cards,2);
-    //    echo '<img src="deckOfCards/'.$random_cards[0].'.png" class="player-hand"></br>';
-
+       // Two random cards in a session variable
        $_SESSION['card0'] = $random_cards[0];
        $_SESSION['card1'] = $random_cards[1];
+       //Score is the sum of value of of the two cards
         $this->score = $this->cards[$random_cards[0]] + $this->cards[$random_cards[1]];
-        $_SESSION['scorePlayer'] = $this->score;
-    //    echo '<img src="deckOfCards/'.$random_cards[1].'.png" class="player-hand"></br>';
+       $_SESSION['scorePlayer'] = $this->score;
 
+       //Dealer
        $_SESSION['card3'] = $random_cards00[0];
        $_SESSION['card4'] = $random_cards00[1];
        $this->score = $this->cards[$random_cards00[0]] + $this->cards[$random_cards00[1]];
        $_SESSION['scoreDealer'] = $this->score;
-
     }
 
-
-    function Hit(){
-
+    public function Hit(){
+      $newCard = array_rand($this->cards, 1);
+      $_SESSION['newCard'] = $newCard;
     }
 
     function Stand(){
--- Blackjack/game.php
+++ Blackjack/game.php
@@ -1,20 +1,12 @@
 <?php
-session_start();
-include 'blackJack.php';
+session_start(); //initialize sessions, needs to be on top of page
+include 'blackJack.php'; //include the class
+//initialize variables and session variables
 $startDeal = false;
 $showDeal = false;
 $showDeal = $_SESSION['showDeal'];
-$startDeal = $_SESSION['startDeal'];
-
-if ($startDeal == false){
-    $_SESSION['playerOne'] = new blackJack();
-
-    $_SESSION['dealer'] = new blackJack();
-
-
-    $_SESSION['startDeal'] = true;
-}
-
+$playerOne = new blackJack();
+$_SESSION['dealer'] = new blackJack();
 ?>
 
 <!DOCTYPE html>
@@ -38,12 +30,12 @@ if ($startDeal == false){
                         <input type="submit" name="quit" value="Quit game">
                     </form>
                 </div>
-                <div class="cardDealer">cardD
+                <div class="cardDealer">
                     <?php
-                    if ($showDeal == false) {
+                    if ($showDeal == false) { //check first time run
                         $_SESSION['showDeal'] = true;
                         $showDeal = true;
-                        $_SESSION['dealer']->Deal();
+                        $_SESSION['dealer']->Deal(); // execute method Deal from class blackjack
                         echo '<img src="deckOfCards/' . $_SESSION['card3'] .'.png" class="player-hand">';
                         echo '<img src="deckOfCards/' . $_SESSION['card4'] .'.png" class="player-hand">';
 
@@ -55,18 +47,24 @@ if ($startDeal == false){
                 </div>
             </div>
             <div class="PC">
-                <div class="cardPlayer">cardP
+                <div class="cardPlayer">
                     <?php
-                    if ($showDeal == false) {
+                    if ($showDeal == false) {  //check first time run
                         $_SESSION['showDeal'] = true;
                         $showDeal = true;
-                        $_SESSION['playerOne']->Deal();
+                        $playerOne->Deal(); // execute method Deal from class blackjack
 
 
                     } else {
 
                         echo '<img src="deckOfCards/' . $_SESSION['card0'] .'.png" class="player-hand">';
                         echo '<img src="deckOfCards/' . $_SESSION['card1'] .'.png" class="player-hand">';
+                        if (isset($_POST['player-hit'])) {
+
+                            $playerOne->Hit();
+                            echo '<img src="deckOfCards/' . $_SESSION['newCard'] .'.png" class="player-hand">';
+                        }
+
                     }
 
                     ?>
@@ -84,9 +82,11 @@ if ($startDeal == false){
 
         </div>
         <div id="buttons">
-            <button id="hit">hit</button>
-            <button id="stand">stand</button>
-            <button id="surrender">give up</button>
+            <form method="post" action="">
+                <input type="submit" name="player-hit" value="hit">
+                <input type="submit" name="player-stand" value="stand">
+                <input type="submit" name="player-surrender" value="surrender">
+            </form>
             <div id="scorePlayer">scorePlayer
                 <?php echo $_SESSION['scorePlayer']; ?>
             </div>
