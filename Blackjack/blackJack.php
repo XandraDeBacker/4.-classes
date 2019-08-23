738a501aa4dfec7b37013369cc1bfc37c70ca7df
patricklibert <patrick.libert@gmail.com>
2019-08-23 16:59:51 +0200

    Third Card

--- Blackjack/blackJack.php
+++ Blackjack/blackJack.php
@@ -7,6 +7,7 @@ class blackJack {
     public $random_cards;
     public $random_cards1;
     public $turn;
+    public $indexcard = 0;
 
     // array of playing cards
     public $cards = array("2C"=>2,"3C"=>3,"4C"=>4,"5C"=>5,"6C"=>6,"7C"=>7,"8C"=>8,"9C"=>9,"10C"=>10,"JC"=>10,"QC"=>10,"KC"=>10,"AC"=>11,
